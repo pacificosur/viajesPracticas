0bdef3a4cfbc8f0d41c89a9fd626beedb22c846f
mireia <mire12car34@gmail.com>
2018-03-15 23:57:16 -0600

    Se subieron archivos en el paquete master, estos se deben borrar

---
+++ src/com/viajespracticas/domain/Empresa.java
@@ -0,0 +1,19 @@
+public class Empresa{
+	private int idEmpresa;
+	private String nombre;
+	private String categoria;
+	private String gerente;
+	private String direccionId;
+	private date fechaCreacion;
+	private date fechaActualizacion;
+	private date fechaEliminacion;
+	private int contactoId;
+
+	public int getIdEmpresa(){
+		return this.idEmpresa;
+	}
+
+	public void setEmpresa(){
+}
+}
+
