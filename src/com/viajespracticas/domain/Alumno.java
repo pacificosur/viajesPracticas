e7fc3bbd185ca9826bbb81b2ceba1992765eee1d
AGARCIA <garcia_0806.g@hotmail.com>
2018-03-15 23:57:42 -0600

    Se subieron archivos en el paquete master, estos archivos deben borrarse

---
+++ src/com/viajespracticas/domain/Alumno.java
@@ -0,0 +1,23 @@
+public class Alumno {
+	private int idAlumno;
+	private String nombre;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	private String matricula;
+	private String semestre;
+	private String carrera;
+	private int edad;
+	private String email;
+	private date fechanNacimiento;
+	private date fechaCreacion;
+	private date fechaActualizacion;
+	private date fechaEliminacion;
+
+
+	public int getIdAlumno(){
+		return this.isAlumno;
+	}
+
+	public void setIdAlumno(int id){
+		this.idAlumno = id;
+}
