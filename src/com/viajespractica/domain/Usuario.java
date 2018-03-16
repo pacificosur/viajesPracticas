4e6fab864f1a96057207c2cd00a840c03498d663
jellyfish <rolando.pedro.gabriel@gmail.com>
2018-03-16 13:12:34 -0600

    Subiendo el POJO usuario

---
+++ src/com/viajespractica/domain/Usuario.java
@@ -0,0 +1,13 @@
+/*------------------------------------------------------------*/
+/* Autor: Rolando Pedro Gabriel                               */
+/* Fecha: 16/03/2018                                          */
+/* Descripción: Clase para modelar la entidad de Usuario      */
+/* Correo: rolando.pedro.gabriel@gmail.com                    */
+/*------------------------------------------------------------*/
+
+
+public class Usuario {
+	private int idUsuario;
+	private String nombre;
+	private String apellidoPaterno;
+}
