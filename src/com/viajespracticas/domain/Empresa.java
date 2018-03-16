e5e988b6cac60e85c31c57df93bb05f3ee47153d
mireia <mire12car34@gmail.com>
2018-03-16 00:27:32 -0600

    POJO, Empresa y Hotel

--- src/com/viajespracticas/domain/Empresa.java
+++ src/com/viajespracticas/domain/Empresa.java
@@ -1,19 +1,91 @@
-public class Empresa{
-	private int idEmpresa;
-	private String nombre;
-	private String categoria;
-	private String gerente;
-	private String direccionId;
-	private date fechaCreacion;
-	private date fechaActualizacion;
-	private date fechaEliminacion;
-	private int contactoId;
 
-	public int getIdEmpresa(){
-		return this.idEmpresa;
-	}
+public class Empresa {
+    
+   private int idEmpresa;
+   private String nombre;
+   private String categoria;
+   private String gerente;
+   private int direccionId;
+   private Date fechaCreacion;
+   private Date fechaAtualizacion;
+   private Date fechaEliminacion;
+   private int contactoId;
 
-	public void setEmpresa(){
+   public Empresa(){
+   
+   }
+
+    public int getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(int idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getGerente() {
+        return gerente;
+    }
+
+    public void setGerente(String gerente) {
+        this.gerente = gerente;
+    }
+
+    public int getDireccionId() {
+        return direccionId;
+    }
+
+    public void setDireccionId(int direccionId) {
+        this.direccionId = direccionId;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaAtualizacion() {
+        return fechaAtualizacion;
+    }
+
+    public void setFechaAtualizacion(Date fechaAtualizacion) {
+        this.fechaAtualizacion = fechaAtualizacion;
+    }
+
+    public Date getFechaEliminacion() {
+        return fechaEliminacion;
+    }
+
+    public void setFechaEliminacion(Date fechaEliminacion) {
+        this.fechaEliminacion = fechaEliminacion;
+    }
+
+    public int getContactoId() {
+        return contactoId;
+    }
+
+    public void setContactoId(int contactoId) {
+        this.contactoId = contactoId;
+    }
+
+    
 }
-}
-
---
+++ src/com/viajespracticas/domain/Hotel.java
@@ -0,0 +1,126 @@
+
+public class Hotel {
+
+    private int idHotel;
+    private String nombre;
+    private int direccionId;
+    private int numerocuartos;
+    private String servicios;
+    private double costo;
+    private String telefono;
+    private String correoElectronico;
+    private String sitioWeb;
+    private String director;
+    private Date fechaCreacion;
+    private Date fechaAtualizacion;
+    private Date fechaEliminacion;
+
+    public Hotel(){
+    
+    }
+    
+    public int getIdHotel() {
+        return idHotel;
+    }
+
+    public void setIdHotel(int idHotel) {
+        this.idHotel = idHotel;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDireccionId() {
+        return direccionId;
+    }
+
+    public void setDireccionId(int direccionId) {
+        this.direccionId = direccionId;
+    }
+
+    public int getNumerocuartos() {
+        return numerocuartos;
+    }
+
+    public void setNumerocuartos(int numerocuartos) {
+        this.numerocuartos = numerocuartos;
+    }
+
+    public String getServicios() {
+        return servicios;
+    }
+
+    public void setServicios(String servicios) {
+        this.servicios = servicios;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getSitioWeb() {
+        return sitioWeb;
+    }
+
+    public void setSitioWeb(String sitioWeb) {
+        this.sitioWeb = sitioWeb;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaAtualizacion() {
+        return fechaAtualizacion;
+    }
+
+    public void setFechaAtualizacion(Date fechaAtualizacion) {
+        this.fechaAtualizacion = fechaAtualizacion;
+    }
+
+    public Date getFechaEliminacion() {
+        return fechaEliminacion;
+    }
+
+    public void setFechaEliminacion(Date fechaEliminacion) {
+        this.fechaEliminacion = fechaEliminacion;
+    }
+    
+}
