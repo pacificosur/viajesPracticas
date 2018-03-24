8d0f47dd4a9689ab3a31100e74f1f308e7f3addf
jellyfish <rolando.pedro.gabriel@gmail.com>
2018-03-23 19:00:36 -0600

    Se actulizaron las fechas de creacion y modificacion de las clases

--- src/com/viajespracticas/dao/UsuarioDao.java
+++ src/com/viajespracticas/dao/UsuarioDao.java
@@ -1,3 +1,11 @@
+/*------------------------------------------------------------*/
+/* Autor: Rolando Pedro Gabriel                               */
+/* Fecha: 22/03/2018                                          */
+/* Fecha Modificación: 23/03/2018                             */
+/* Correo: rolando.pedro.gabriel@gmail.com                    */
+/* Descripción: Clase para modelar la interface Usuario       */
+/*------------------------------------------------------------*/
+
 public interface UsuarioDao {
 	public List<Usuario> obtenerRegistros();
 	public Usuario obtenerRegistro(Integer id);
